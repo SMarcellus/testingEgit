f96b92c2176838a7c8bbb7ba351984c031a70289
Suzanne <suzanne@marcellus.org>
2015-12-31 11:52:30 -0500

    adding another class to see how that shows up

---
+++ tryJavaJRE/src/tryJavaJRE/SecondHellowClass.java
@@ -0,0 +1,13 @@
+package tryJavaJRE;
+
+public class SecondHellowClass {
+
+	
+	public void printAnotherHello()
+	{
+		String nextline = "A second hello world class?";	
+		System.out.println(nextline);
+	}
+	
+
+}
